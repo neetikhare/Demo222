3adc67719de163b317746478b439216568207502
neetikhare <114065620+neetikhare@users.noreply.github.com>
2022-09-21 17:26:08 +0530

    Add files via upload

---
+++ PrintNumber.java
@@ -0,0 +1,12 @@
+package in.cdac.assignment3;
+
+public class PrintNumber {
+ 	public static void main(String[] args) {
+ 		System.out.println("between 1 to 100 numbers are");
+		for(int a=1;a<=100;a++)
+			System.out.print(" " +a);
+		
+
+	}
+
+}
